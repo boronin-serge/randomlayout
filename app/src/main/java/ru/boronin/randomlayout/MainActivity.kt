26e533fd703a4fc88204d004967fd0abf886cf4a
Sergey Boronin <boronin-serge@ya.ru>
2021-02-03 17:20:04 +0300

    Initial commit

---
+++ app/src/main/java/ru/boronin/randomlayout/MainActivity.kt
@@ -0,0 +1,18 @@
+package ru.boronin.randomlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.boronin.randomlayout.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.main_activity)
+    if (savedInstanceState == null) {
+      supportFragmentManager.beginTransaction()
+        .replace(R.id.container, MainFragment.newInstance())
+        .commitNow()
+    }
+  }
+}
